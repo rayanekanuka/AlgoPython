288b1f30253b25ef440b71affe3bf6b6a77174a8
rayane.kanuka <rayane.kanuka@WJS-13>
2024-02-12 11:36:09 +0100

    Algo de tri selection done

--- Algo/tri_selection.py
+++ Algo/tri_selection.py
@@ -4,15 +4,16 @@ def swap(list, i, j):
     list[i], list[j] = list[j], list[i]
 
 def tri_selection(list):
-    print("\ntri_selection\n", list)
-    for i in range(1, len(list)):
-        min_index = list[i]
-        j = i
-        while j > 0 and list[j - 1] < min_index:
-            list[j] = list[j - 1]
-            j = j - 1
-        list[j] = min_index
+    print("\ntri_selection\n", "liste : ", list)
+    length = len(list)
+    for i in range(0, length):
+        min = i
+        for j in range(i+1, length):
+            if list[j] < list[min]:
+                min = j
+        swap(list, i, min)
+        print("chaque tour de boucle :", list)
     return list
 
 
-print(tri_selection(list))
+print("résultat : ",tri_selection(list))
