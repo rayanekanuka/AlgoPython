7dc2897c17d2e7ea890c4394523e6f4c313a7740
rayane.kanuka <rayane.kanuka@WJS-13>
2024-02-12 16:57:34 +0100

    Recursive algo fibonacci et syracuse

--- Algo/AlgoTri/recursivity.py
+++ Algo/AlgoTri/recursivity.py
@@ -10,3 +10,24 @@ resultat = factorielle(n)
 print(resultat)   # ici 120
 
 
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    if n > 1:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(8))
+
+
+def syracuse(n):
+    print(n)
+    if n > 1:
+        if n%2 == 0:
+            return syracuse(n/2)
+        else:
+            return syracuse(3 * n + 1)
+
+print(syracuse(15))
+
